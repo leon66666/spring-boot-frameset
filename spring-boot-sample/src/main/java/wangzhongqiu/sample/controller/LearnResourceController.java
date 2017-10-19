21138d062de9414dd37b0c90d7c82410833ae5c3
leon66666 <1902518544@qq.com>
2017-10-19 17:44:20 +0800

    first version

--- spring-boot-sample/src/main/java/wangzhongqiu/sample/controller/LearnResourceController.java
+++ spring-boot-sample/src/main/java/wangzhongqiu/sample/controller/LearnResourceController.java
@@ -1,18 +1,12 @@
 package wangzhongqiu.sample.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import wangzhongqiu.sample.domain.LearnResouce;
-import wangzhongqiu.sample.domain.User;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author wangzhongqiu
---
+++ spring-boot-sample/src/main/java/wangzhongqiu/sample/controller/LogController.java
@@ -0,0 +1,27 @@
+package wangzhongqiu.sample.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wangzhongqiu
+ * @date 2017/10/19.
+ */
+@RestController
+@RequestMapping("/log")
+public class LogController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/logtest")
+    public String logtest() {
+        //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+        logger.trace("日志输出 trace");
+        logger.debug("日志输出 debug");
+        logger.info("日志输出 info");
+        logger.warn("日志输出 warn");
+        logger.error("日志输出 error");
+        return "访问成功，日志已经成功打印";
+    }
+}
