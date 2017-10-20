5ebb50b17bd38f4b8eb5f9858ad1f2ee9d5ec717
leon66666 <1902518544@qq.com>
2017-10-20 16:33:14 +0800

    first version

---
+++ test/src/main/java/wangzhongqiu/springboot/jdbc/dao/LearnDao.java
@@ -0,0 +1,17 @@
+package wangzhongqiu.springboot.jdbc.dao;
+
+import wangzhongqiu.springboot.jdbc.domain.LearnResouce;
+import wangzhongqiu.springboot.jdbc.tools.Page;
+
+import java.util.Map;
+
+/**
+ * Created by tengj on 2017/4/8.
+ */
+public interface LearnDao {
+    int add(LearnResouce learnResouce);
+    int update(LearnResouce learnResouce);
+    int deleteByIds(String ids);
+    LearnResouce queryLearnResouceById(Long id);
+    Page queryLearnResouceList(Map<String,Object> params);
+}
---
+++ test/src/main/java/wangzhongqiu/springboot/jdbc/service/LearnService.java
@@ -0,0 +1,17 @@
+package wangzhongqiu.springboot.jdbc.service;
+
+import wangzhongqiu.springboot.jdbc.domain.LearnResouce;
+import wangzhongqiu.springboot.jdbc.tools.Page;
+
+import java.util.Map;
+
+/**
+ * Created by tengj on 2017/4/7.
+ */
+public interface LearnService {
+    int add(LearnResouce learnResouce);
+    int update(LearnResouce learnResouce);
+    int deleteByIds(String ids);
+    LearnResouce queryLearnResouceById(Long learnResouce);
+    Page queryLearnResouceList(Map<String,Object> params);
+}
