24acbb6206a3cf25723928f73f2e517044589917
leon66666 <1902518544@qq.com>
2017-10-23 11:13:18 +0800

    first version

---
+++ untitled/src/main/java/wangzhongqiu/springboot/jdbc/service/impl/LearnServiceImpl.java
@@ -0,0 +1,44 @@
+package wangzhongqiu.springboot.jdbc.service.impl;
+
+import wangzhongqiu.springboot.jdbc.dao.LearnDao;
+import wangzhongqiu.springboot.jdbc.domain.LearnResouce;
+import wangzhongqiu.springboot.jdbc.service.LearnService;
+import wangzhongqiu.springboot.jdbc.tools.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * Created by tengj on 2017/4/7.
+ */
+@Service
+public class LearnServiceImpl implements LearnService {
+
+    @Autowired
+    LearnDao learnDao;
+    @Override
+    public int add(LearnResouce learnResouce) {
+        return this.learnDao.add(learnResouce);
+    }
+
+    @Override
+    public int update(LearnResouce learnResouce) {
+        return this.learnDao.update(learnResouce);
+    }
+
+    @Override
+    public int deleteByIds(String ids) {
+        return this.learnDao.deleteByIds(ids);
+    }
+
+    @Override
+    public LearnResouce queryLearnResouceById(Long id) {
+        return this.learnDao.queryLearnResouceById(id);
+    }
+
+    @Override
+    public Page queryLearnResouceList(Map<String,Object> params) {
+        return this.learnDao.queryLearnResouceList(params);
+    }
+}
