69f52c160d15a93e2c3a314855b1439e1ce7e7c2
leon66666 <1902518544@qq.com>
2017-10-31 18:31:03 +0800

    first version

---
+++ spring-boot-amqp/src/main/java/wangzhongqiu/springboot/amqp/SpringBootAmqpApplication.java
@@ -0,0 +1,12 @@
+package wangzhongqiu.springboot.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAmqpApplication.class, args);
+	}
+}
