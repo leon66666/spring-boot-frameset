e21073ce778ab387fcef7e2ec0c29a0de0dd31b0
leon66666 <1902518544@qq.com>
2017-11-08 17:13:55 +0800

    first version

--- spring-boot-amqp/src/main/java/wangzhongqiu/springboot/amqp/SpringBootAmqpApplication.java
+++ spring-boot-amqp/src/main/java/wangzhongqiu/springboot/amqp/SpringBootAmqpApplication.java
@@ -1,12 +1,73 @@
 package wangzhongqiu.springboot.amqp;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootAmqpApplication {
+    final static String queueName = "spring-boot";
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootAmqpApplication.class, args);
-	}
+    final static String HOST = "120.27.114.229";
+
+    final static String USERNAME = "root";
+
+    final static String PASSWORD = "root";
+
+    final static int PORT = 5672;
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost(HOST);
+        connectionFactory.setPort(PORT);
+        connectionFactory.setUsername(USERNAME);
+        connectionFactory.setPassword(PASSWORD);
+        connectionFactory.setVirtualHost("/");
+        //回调
+        connectionFactory.setPublisherConfirms(true);
+        return connectionFactory;
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SpringBootAmqpApplication.class, args);
+    }
 }
